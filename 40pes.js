52a4a262e03d366b8ec28302d12d54af5a4d9a14
GIOVANNI OLIVEIRA DE GODOY <giovanni.ogodoy@senacsp.edu.br>
2024-05-29 23:25:05 -0300

    colocando a box de containers usados e semi-novos para funcionar com os botõe para passar a imagem

--- 40pes.js
+++ 40pes.js
@@ -16,3 +16,53 @@ window.onclick = function(event) {
     }
   }
 }
+
+
+//função para fazer a box "containers usados" funcionar  
+let currentImage = 0;
+const images = document.querySelectorAll('.ContainersUsados img');
+const captions = document.querySelectorAll('.ContainersUsados figcaption');
+
+function showImage(index) {
+    images.forEach((img, i) => {
+        img.style.display = i === index ? 'block' : 'none';
+        captions[i].style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextImage() {
+    currentImage = (currentImage + 1) % images.length;
+    showImage(currentImage); //função para mostrar a imagem que vem a seguir 
+}
+
+function lastImage() {
+  currentImage = (currentImage - 1 + images.length) % images.length;
+  showImage(currentImage); //função para mostrar a imagem anterior da que esta sendo mostrada na tela
+}
+// Exibe a primeira imagem ao carregar a página
+showImage(currentImage);
+
+
+//função para fazer a box "containers semi-novos" funcionar  
+let ImagemAtual = 0;
+const imagens = document.querySelectorAll('.ContainersSemiNovos img');
+const descricao = document.querySelectorAll('.ContainersSemiNovos figcaption');
+
+function mostrarImagem(index) {
+    imagens.forEach((img, i) => {
+        img.style.display = i === index ? 'block' : 'none';
+        descricao[i].style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function ProximaImagem() {
+    ImagemAtual = (ImagemAtual + 1) % imagens.length;
+    mostrarImagem(ImagemAtual); //função para mostrar a imagem que vem a seguir 
+}
+
+function UltimaImagem() {
+  ImagemAtual = (ImagemAtual - 1 + imagens.length) % imagens.length;
+  mostrarImagem(ImagemAtual); //função para mostrar a imagem anterior da que esta sendo mostrada na tela
+}
+// Exibe a primeira imagem ao carregar a página
+mostrarImagem(ImagemAtual);

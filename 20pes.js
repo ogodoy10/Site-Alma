a57cfdfeed98aa4cba485f804675c6389ceeda8c
GIOVANNI OLIVEIRA DE GODOY <giovanni.ogodoy@senacsp.edu.br>
2024-06-08 01:05:20 -0300

    colocando as boxes para mostrar os containers e editando elas

--- 20pes.js
+++ 20pes.js
@@ -16,3 +16,75 @@ window.onclick = function(event) {
     }
   }
 }
+
+
+
+//função para fazer a box "containers usados" funcionar  
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.image-container20');
+const container = document.querySelector('.ContainersUsados');
+
+function showImage(index) {
+    images.forEach((img, i) => {
+        if (i === index) {
+            img.classList.add('active');
+            if (img.querySelector('img').id === 'img3') {
+                container.style.height = '810px';  // Ajuste a altura conforme necessário
+            } else {
+                container.style.height = '565px';
+            }
+        } else {
+            img.classList.remove('active');
+        }
+    });
+}
+
+function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    showImage(currentImageIndex);
+}
+
+function lastImage() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    showImage(currentImageIndex);
+}
+
+// Mostrar a primeira imagem ao carregar a página
+showImage(currentImageIndex);
+
+
+
+
+//função para fazer a box "containers semi-novos" funcionar  
+// Containers Semi-Novos
+let currentImageIndexSemiNovo = 0;
+const imagesSemiNovo = document.querySelectorAll('.imageContainer-SemiNovo');
+const containerSemiNovo = document.querySelector('.ContainersSemiNovos');
+
+function showImageSemiNovo(index) {
+    imagesSemiNovo.forEach((img, i) => {
+        if (i === index) {
+            img.classList.add('ativa');
+            if (img.querySelector('img').id === 'img3') {
+                containerSemiNovo.style.height = '810px';  // Ajuste a altura conforme necessário
+            } else {
+                containerSemiNovo.style.height = '565px';
+            }
+        } else {
+            img.classList.remove('ativa');
+        }
+    });
+}
+
+function nextImageSemiNovo() {
+    currentImageIndexSemiNovo = (currentImageIndexSemiNovo + 1) % imagesSemiNovo.length;
+    showImageSemiNovo(currentImageIndexSemiNovo);
+}
+
+function lastImageSemiNovo() {
+    currentImageIndexSemiNovo = (currentImageIndexSemiNovo - 1 + imagesSemiNovo.length) % imagesSemiNovo.length;
+    showImageSemiNovo(currentImageIndexSemiNovo);
+}
+
+// Mostrar a primeira imagem ao carregar a página
+showImageSemiNovo(currentImageIndexSemiNovo);
